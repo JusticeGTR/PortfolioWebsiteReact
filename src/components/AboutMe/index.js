a5767ee902d005b50ecd8bb523aedbb9b65f3915
Justin Lindsey <justinlindseylhr@gmail.com>
2021-06-07 15:20:39 -0500

    picture fix maybe

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -6,7 +6,9 @@ export function AboutMe() {
         <h2 className=" col rightBorder rotate">About Me</h2>
         <div className="col">
         <p><span><img src={profile}
-            alt="Artist and his dog resting her head on his shoulder" className=" col me" /></span><span>I am an artist turned web developer. After studying graphic design in college and dabbling in most other mediums, I decided to try something new. A friend suggested to try out coding, and I fell into the deep end immediately. I want to build things to help others and make them smile, and in at least that small way, help develop our society into something we can all be proud of.
+            alt="Artist and his dog resting her head on his shoulder" className=" col me" /></span><span>
+                NEW TEXT IS UPDATING YAY
+                I am an artist turned web developer. After studying graphic design in college and dabbling in most other mediums, I decided to try something new. A friend suggested to try out coding, and I fell into the deep end immediately. I want to build things to help others and make them smile, and in at least that small way, help develop our society into something we can all be proud of.
                 I love traveling, reading, creating and a wide variety of other things. </span>
         </p>
         </div>
